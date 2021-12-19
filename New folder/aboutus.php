b886e91d214bdcbc66f1d13d3d183adc23436782
Kobigan <kobitech18@gmail.com>
2021-12-19 22:34:28 +0530

    added

---
+++ New folder/aboutus.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Hotel Reservation system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css26/style.css">
+	<script src="assets/js/script5.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<body background="assets/images/11568168178_tmp_bricks-brickwall-brickwork-276514.jpg" >
+   
+   	<!--INCLUDING HEADER AND THE NEVIGATION --->
+	<header id="header"> <?php include('includes/header.php');?> </header>
+	<nev id="neVV2"> <?php include('includes/nevigation.php');?> </nev>
+
+<!--PAGE WITH FOOTER-FOOTER-POSITIONING-->
+<div id="page-container" >
+  
+   <!--PAGE CONTENT-->
+   <div id="content-wrap" style="padding-top:150px; padding-left:20px; ">
+    <div class="fade" id="logformbackgr2" style="background-color: rgb(0,0,0,0.6);position:absolute;z-index: 1;color:white;height: 1420px;">
+    <!--h1 style="text-align: center;color:rgb(255,255,255,0.7);font-weight: normal;font-family: monospace;font-size: auto;margin-top: 20px;margin-bottom: 0px;" id="about">ABOUT HOTEL LUSTRIO INN</h1>
+            <hr style="width: 86%;"-->
+    
+    <div id="slider">
+        <figure class="fade">
+
+            <img src="assets/images/computer-2982270_1920 (1) (1).jpg">
+            <img src="assets/images/computer-2982270_1920 (1) (1).jpg">
+            <img src="assets/images/computer-2982270_1920 (1) (1).jpg">
+            <img src="assets/images/computer-2982270_1920 (1) (1).jpg">
+            <img src="assets/images/computer-2982270_1920 (1) (1).jpg">
+
+        </figure>
+        <h3 style="text-align: center;font-size: 45px;position: absolute;top: 30px;left:25%;width: 50%;color: black;" id="fam">HOTEL LUSTRIO INN is now part of the HostPapa family.</h3>
+        <div id="str" style="text-align: center;font-size: 45px;position: absolute;top: 180px;left:13%;width: 70%;color: black; height: 15%;background-color: rgb(255,255,255,0.4);">
+          <h6 style="color: white;font-size: 20px;text-align: center;color: black;font-family: monospace;">All Lunarpages customers will receive all the benefits of being part of the HostPapa family, including enhanced customer support, upgraded state‑of‑the‑art hosting infrastructure and more.
+
+<label style="color: green;">If you're an existing Lunarpages customer, learn more about HostPapa with our FAQ.</label> If you're not a customer yet, check out the smart solutions we offer to power your small business.</h6>
+
+        </div>
+        <div id="str2" style="text-align: center;font-size: 35px;position: absolute;top:13px;left:6%;width: 90%;color: black; height: 15%;background-color: rgb(255,255,255,0.4);">
+          <h6 style="color: white;font-size: 17px;text-align: center;color: black;font-family: monospace;">All Lunarpages customers will receive all the benefits of being part of the HostPapa family, including enhanced customer support, upgraded state‑of‑the‑art hosting infrastructure and more.
+
+<label style="color: green;">If you're an existing Lunarpages customer, learn more about HostPapa with our FAQ.</label> If you're not a customer yet, check out the smart solutions we offer to power your small business.</h6>
+
+        </div>
+    </div>
+
+    <h3 style="color:rgb(255,255,255,0.7);font-weight: normal;font-family: monospace;margin-top: 20px;" id="detfont"> The story of HOTEL LUSTRIO INN Colombo which opened its doors in 1984 is a splendid tale of continual improvement of product and the highest standards of quality in hospitality. Having embraced over 3 decades of expertise in hospitality our vision and beliefs are firmly grounded in extending a true personalized service to all our guests, laced with an unforgettable luxury hotel experience. The brand has enticed many elite personalities from around the world including heads of government, prime ministers of leading nations, royalty, well known sports and entertainment personalities and many more. </h3>
+    <h1 style="text-align: center;color:rgb(255,255,255,0.7);font-weight: normal;font-family: monospace;text-decoration: underline;"> Places to See </h1>
+    <table border="0" style="width: 100%; text-align: left;color:rgb(0,0,0,0.7);font-weight: normal;font-family: monospace;margin-top: 20px;background-color: rgb(255,255,255,0.7);"  id="detfont">
+    <tr >
+      <th>Place</th>
+      <th>Discription</th>
+    </tr>
+    <tr>
+      <td>Galle Face Green</td>
+      <td>A wide expanse of green facing the Indian Ocean that is ideal for picnics and other outdoor activities</td>
+
+    </tr>
+    <tr>
+      <td>Royal Colombo Golf Club</td>
+      <td>Second oldest Royal Golf Club outside the United Kingdom which was founded over 130 years ago</td>
+
+    </tr>
+    <tr>
+      <td>Independence Square</td>
+      <td>A national monument built for commemoration of the independence of Sri Lanka reflecting distinct Sri Lankan architecture</td>
+
+    </tr>
+    <tr>
+      <td>National Museum</td>
+      <td>A rich reservoir of ancient relics dating back when texts were written on palm leaves</td>
+
+    </tr>
+    <tr>
+      <td>Pettah</td>
+      <td>A commercial district with streets well-known for selling clothing, electronics, and limitless knickknacks at very affordable prices</td>
+
+    </tr>
+    <tr>
+      <td>The Fort</td>
+      <td>Housing offices, shops and hotels, some of which exuding old world charm with colonial architecture</td>
+
+    </tr>
+    <tr>
+      <td>Gangarama Temple</td>
+      <td>A picturesque temple comprising the best of architecture which has been in existence for over 120 years</td>
+
+    </tr>
+    <tr>
+      <td>Viharamahadevi Park</td>
+      <td>The city's largest park, luring visitors with its beautiful landscape</td>
+
+    </tr>
+     <tr>
+      <td>Kelaniya Raja Maha Vihara</td>
+      <td>A famous Buddhist temple which serves as a venue for a religious procession</td>
+
+    </tr>
+    </table>
+    </div>
+    
+    
+   </div>
+    
+   <footer id="footer" style="position: absolute;top: 1431px;background-color: rgb(255,255,255,0.9);z-index: 1;"> <?php include('includes/footer.php');?> </footer>
+</div>
+</body>
+</html>

bdaf0b6a60ec8e712a49a0f86322180d02a6cd6d
Kobigan <kobitech18@gmail.com>
2021-12-19 22:46:54 +0530

    added

---
+++ New folder/pay.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Hotel Reservation system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css26/style.css">
+	<script src="assets/js/script5.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<body background="assets/images/marina-bay-sands-1920x1080-infinity-pool-pool-hotel-travel-booking-336.jpg" >
+   
+   	<!--INCLUDING HEADER AND THE NEVIGATION --->
+	<header id="header"> <?php include('includes/header.php');?> </header>
+	<nev id="neVV2"> <?php include('includes/nevigation.php');?> </nev>
+
+<!--PAGE WITH FOOTER-FOOTER-POSITIONING-->
+<div id="page-container" >
+  
+   <!--PAGE CONTENT-->
+   <div id="content-wrap" style="padding-top:150px; padding-left:20px; ">
+     <!--
+
+     	ALL THE PAGE CONTENT
+     	***************************************************************
+     	***************************************************************
+     	***************************************************************
+     	ALL THE PAGE CONTENT
+     	***************************************************************
+     	***************************************************************
+     	***************************************************************
+     	ALL THE PAGE CONTENT
+     	***************************************************************
+     	***************************************************************
+     	***************************************************************
+     	ALL THE PAGE CONTENT
+     	***************************************************************
+     	***************************************************************
+     	***************************************************************
+     -->
+    
+   </div>
+    
+   <footer id="footer" style="position: absolute;top: 1051px;background-color: rgb(255,255,255,0.9);"> <?php include('includes/footer.php');?> </footer>
+</div>
+</body>
+</html>

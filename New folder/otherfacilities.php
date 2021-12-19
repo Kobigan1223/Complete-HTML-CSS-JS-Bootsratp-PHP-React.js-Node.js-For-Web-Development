98578efacab63d55f7fe90202b817fa9d264216e
Kobigan <kobitech18@gmail.com>
2021-12-19 22:44:48 +0530

    added

---
+++ New folder/otherfacilities.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Hotel Reservation system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css26/style.css">
+	<script src="assets/js/script5.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<body background="assets/images/11568168178_tmp_bricks-brickwall-brickwork-276514.jpg" >
+   
+   	<!--INCLUDING HEADER AND THE NEVIGATION --->
+	<header id="header"> <?php include('includes/header.php');?> </header>
+	<nev id="neVV2"> <?php include('includes/nevigation.php');?> </nev>
+
+<!--PAGE WITH FOOTER-FOOTER-POSITIONING-->
+<div id="page-container" >
+  
+   <!--PAGE CONTENT-->
+   <div id="content-wrap" style="padding-top:150px; padding-left:20px; ">
+    <div class="fade" id="logformbackgr2" style="background-color: rgb(0,0,0,0.6);position:absolute;z-index: 1;color:white;height: 920px;">
+    <h1 style="text-align: center;color:rgb(255,255,255,0.7);font-weight: normal;font-family: monospace;font-size: 35px;margin-top: 20px;margin-bottom: 0px;">OUR FACILITIES</h1>
+            <hr style="width: 86%;">
+
+    </div> 
+     
+   </div>
+    
+   <footer id="footer" style="position: absolute;top: 1051px;background-color: rgb(255,255,255,0.9);"> <?php include('includes/footer.php');?> </footer>
+</div>
+</body>
+</html>

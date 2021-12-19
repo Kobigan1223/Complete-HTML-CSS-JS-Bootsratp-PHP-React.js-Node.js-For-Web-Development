0e4d187a001fe61b76703df9253e0826fedbaf41
Kobigan <kobitech18@gmail.com>
2021-12-19 22:43:52 +0530

    added

---
+++ New folder/FAQ.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Hotel Reservation system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css26/style.css">
+	<script src="assets/js/script5.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--Remastering-->
+<body background="assets/images/marina-bay-sands-1920x1080-infinity-pool-pool-hotel-travel-booking-336.jpg" >
+   
+   	<!--INCLUDING HEADER AND THE NEVIGATION --->
+	<header id="header"> <?php include('includes/header.php');?> </header>
+	<nev id="neVV"> <?php include('includes/nevigation.php');?> </nev>
+
+<!--PAGE WITH FOOTER-FOOTER-POSITIONING-->
+<div id="page-container" >
+  
+   <!--PAGE CONTENT-->
+   <div id="content-wrap" style="padding-top:150px; padding-left:20px; ">
+   <div class="fade" id="logformbackgr2" style="background-color: rgb(0,0,0,0.7);position:fixed;z-index: 1;color:white;">
+   <h3 style="text-align: center; color:white; font-family: monospace;font-size: 45px;margin-top: 10px; margin-left: 20px;"> FAQ </h3>
+   <h2> 
+   
+      <div  style="width: 100%;">
+      <ul>
+      
+        <li id="Q1">How can I find a room spefic price?</li>
+        
+
+      </ul>
+         <h6 id="qd1" style="margin-left: 40px;margin-top: 0; margin-right: 30px;">In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.</h6>
+
+      </div>
+
+      <div  style="width: 100%;">
+      <ul>
+      
+        <li id="Q2"> How can I find a room spefic price? </li>
+        
+
+      </ul>
+         <h6 id="qd2" style="margin-left: 40px;margin-top: 0; margin-right: 30px;">In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.</h6>
+
+      </div>
+
+      <div  style="width: 100%;">
+      <ul>
+      
+        <li id="Q3"> How can I find a room spefic price?</li>
+        
+
+      </ul>
+         <h6 id="qd3" style="margin-left: 40px;margin-top: 0; margin-right: 30px;">In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.In the top corner you can find search bar. click and and fid the page and the room that you need.</h6>
+
+      </div>
+    
+
+   </h2>
+   <div style="margin-top: 50px;" class="FAQbtns">
+  
+        <button style="margin-left: 47%;width: 20px;border-radius: 5px;" onclick="changeFAQcontent('Q1');">1</button>
+        <button style="margin-left: 1%;width: 20px;border-radius: 5px;" onclick="changeFAQcontent('Q2');">2</button>
+        <button style="margin-left: 1%;width: 20px;border-radius: 5px;" onclick="changeFAQcontent('Q3');">3</button>
+   </div>
+   </div>
+
+
+   
+
+ 
+   </div>
+    
+   <footer id="footer" style="position: absolute;top: 1051px;background-color: rgb(255,255,255,0.9);z-index: 1;"> <?php include('includes/footer.php');?> </footer>
+</div>
+</body>
+</html>

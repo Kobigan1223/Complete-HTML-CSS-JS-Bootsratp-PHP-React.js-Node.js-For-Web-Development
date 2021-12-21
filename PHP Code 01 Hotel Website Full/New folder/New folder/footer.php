d76317b054c3ed45a2fd78b4ad3d5fb0a3d20af3
Kobigan <kobitech18@gmail.com>
2021-12-21 22:04:14 +0530

    Files Updated

---
+++ PHP Code 01 Hotel Website Full/New folder/New folder/footer.php
@@ -0,0 +1,60 @@
+<!--FOOTER-->
+
+
+<section>
+<!--FOOTER-CONTAINER-->
+<div class="footer-container" style="display:block;color: white;" >
+	
+	<!--FOOTER-CONTENTS-->
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 5%;" >
+	<h4 id="footerPOS"><b> &HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Add Rooms</li>
+		<li>Add Resorts</li>
+		
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 30%;" >
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Manage Rooms</li>
+		<li>Manage Resorts</li>
+		
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 55%;" >
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Change Pword</li>
+		
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 80%;" >
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li ><a href="confirmlogout.php" style="color: white;text-decoration: none;">Log Out</a></li>
+		
+	</ul>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:20px; " id="footerimages">
+	<a href="#"><img src="assets/logos/iconfinder_Vector-2_4747497.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:60px; " id="footerimages">
+	<a href="#"><img src="assets/logos/iconfinder_Vector-3_4747496.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:100px; " id="footerimages"> 
+	<a href="#"><img src="assets/logos/iconfinder_Vector-1_4747498.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+
+	<div class="trademark" style="position:absolute; top:110px;left:47%;" >
+	<h4 id="footerPOS"><b>ALL RIGHTS RESERVED </b></h4>
+	
+	</div>
+	
+	
+
+</div>
+</section>
+
+
+<!--END OF FOOTER-->

1f09a458274382733f410726bf6912a2a3081d61
Kobigan <kobitech18@gmail.com>
2021-12-21 22:01:01 +0530

    added

---
+++ PHP Code 01 Hotel Website Full/New folder/footer.php
@@ -0,0 +1,73 @@
+<?php
+  
+  if(isset($_SESSION['login']))
+  {
+    //require('confirmlogout.php');
+
+  }
+  
+
+
+?>
+
+<!--FOOTER-->
+
+<section>
+<!--FOOTER-CONTAINER-->
+<div class="footer-container" style="display:block;color: black;" >
+	
+	<!--FOOTER-CONTENTS-->
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 5%;" >
+	<h4 id="footerPOS" ><b> &HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Rooms</li>
+		<li>Resorts</li>
+		<li>FAQ</li>
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 30%;" >
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Gallery</li>
+		<li>Restaurant</li>
+		<li>Booking</li>
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 55%; ">
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li>Availability</li>
+		<li>Search</li>
+		<li>Facilities</li>
+	</ul>
+	</div>
+	<div class="footer-left-content footer-list" style="position:absolute; top:10px;left: 80%; ">
+	<h4 id="footerPOS"><b> HOTEL LUSTRIO INN </b></h4>
+	<ul>
+		<li><a href="admin/adminlogin.php">Admin login</a></li>
+		
+	</ul>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:20px; " id="footerimages">
+	<a href="#"><img src="assets/logos/iconfinder_Vector-2_4747497.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:60px; " id="footerimages">
+	<a href="#"><img src="assets/logos/iconfinder_Vector-3_4747496.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+	<div style="float:right; position:absolute;right:1%; top:100px; " id="footerimages"> 
+	<a href="#"><img src="assets/logos/iconfinder_Vector-1_4747498.png" height="30px" width="40px" style="margin-right:10px;"></a>
+	</div>
+
+	<div class="trademark" style="position:absolute; top:110px;left:47%; ">
+	<h4 id="footerPOS"><b>ALL RIGHTS RESERVED </b></h4>
+	
+	</div>
+	
+	
+
+</div>
+</section>
+
+
+
+<!--END OF FOOTER-->

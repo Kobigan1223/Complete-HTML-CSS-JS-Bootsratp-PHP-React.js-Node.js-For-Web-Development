649a8ef38f8d6bf0e6f48f069a6e8b6beb9d78d1
Kobigan <kobitech18@gmail.com>
2021-12-21 22:06:46 +0530

    added

---
+++ PHP Code 01 Hotel Website Full/New folder/includes/nevigation.php
@@ -0,0 +1,35 @@
+<script>
+function hide() {
+    
+    document.getElementById("nevigate").style.display = "none";
+    document.getElementById("logformbackgr").style.width = "100%";
+   
+}
+
+</script>
+
+
+
+<section>
+
+<div class="header2 fade" id="nevigate" >
+  	<div class="header-right2" id="axe">
+        <a href="admindashboard.php" id="axe">Dashboard</a>
+        
+	<a href="addroom.php" id="axe">Add Room</a>
+        <a href="managerooms.php" id="axe">Manage Rooms</a>
+	<a href="addpackage.php" id="axe">Add Package</a>
+	<a href="managepackages.php" id="axe">Manage Packages</a>
+	<a href="resevations.php" id="axe">Reservations</a>
+	<a href="registeredusers.php" id="axe">Reg Users</a>
+      <a id="hidemore"  onclick="hide()">Hide</a>
+
+
+
+
+
+    
+  </div>
+</div>
+</section>
+

681ffde39fd1bce25e1cb6e7a48e1734c24d8cca
Kobigan <kobitech18@gmail.com>
2021-12-23 18:38:57 +0530

    added

---
+++ PHP Code 01 Hotel Website Full/New folder/index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Online Hotel Reservation system</title>
+	<link rel="stylesheet" type="text/css" href="assets/css14/style.css">
+	<script src="assets/js/script.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+<body background="assets/images/prism_abstract-1920x1080.jpg" class="fade" >
+   
+   	<!--INCLUDING HEADER AND THE NEVIGATION --->
+	<header id="header"> <?php include('includes/header.php');?> </header>
+	<nev id="neVV"> <?php include('includes/nevigation.php');?> </nev>
+
+<!--PAGE WITH FOOTER-FOOTER-POSITIONING-->
+<div id="page-container" >
+  
+   <!--PAGE CONTENT-->
+
+   <div id="content-wrap" style="padding-top:150px; padding-left:20px; ">
+     
+    
+
+   </div>
+
+
+  	
+
+</div>
+    
+<footer id="footer" style="position: absolute;top: 1079px;background-color: rgb(255, 255, 255,0.1);"> <?php include('includes/footer.php');?> </footer>
+</div>
+</body>
+</html>
